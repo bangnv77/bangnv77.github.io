09ea0985d0eb9988deea947ec16ac6c85415b3de
Bang Nguyen <bangnv77@gmail.com>
2016-12-24 22:49:33 -0500

    add smooth transition between sections

---
+++ js/smoothscroll.js
@@ -0,0 +1,38 @@
+$("#navbar ul li a[href^='#']").on('click', function(e) {
+
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  // animate
+  $('html, body').animate({
+    // scrollTop: $(this.hash).offset().top
+    scrollTop: $(hash).offset().top -50
+  }, 300, function() {
+
+    // when done, add hash to url
+    // (default click behavior)
+    window.location.hash = hash;
+  });
+});
+
+$("a.navbar-brand[href^='#']").on('click', function(e) {
+
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  // animate
+  $('html, body').animate({
+    scrollTop: $(this.hash).offset().top -50
+  }, 700, function() {
+
+    // when done, add hash to url
+    // (default click behavior)
+    window.location.hash = hash;
+  });
+});
